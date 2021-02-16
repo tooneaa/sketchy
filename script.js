0c1b958f4949f650643a94c7cb9dd9585a94f204
tooneaa <tooneaa@hotmail.com>
2021-02-16 22:52:27 +0800

    auto change container width according to grid column

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let main = document.getElementById('container')
 let container = document.createElement('div')
 let grid = document.createElement('div')
 let slot = '<div class="grid-item"></div>'
-let gridBox = ""
+let gridBox = "" // to create the HTML of Divs by chaining slot//
 container.setAttribute('class','container')
 grid.setAttribute('class','grid-item')
 
@@ -13,9 +13,16 @@ grid.setAttribute('class','grid-item')
 function createGrid(x) {
   for (i=0 ; i <x ; i++ ) {
   gridBox += slot}
-
   return gridBox
 }
 
-createGrid(16)
+function gridwidth(x) {
+  let gridwidth = (x/4)*50
+  return gridwidth
+}
+
+let gridNumber = 16;
+let changeWidth = main.style.width = gridwidth(gridNumber) +"px"
+
+createGrid(gridNumber)
 main.innerHTML = gridBox
